a541961993ae77a893ab171ae352448879af79ef
SuryatejaDuvvuri <duvvurisuryateja95@gmail.com>
2023-04-29 16:10:34 -0700

    Changed user input parameters

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,21 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+
   return (
     <main>
         <div className = "flex-col p-6 max-w-sm mx-auto ">
           {/* Carbon calculator */}
           <div className = "container mx-9 bg-black">
           {/* Drop down menu */}
-              <input placeholder='anything'
+              <input placeholder='Money'
               className="border rounded w-full py-2 px-3"
               type="text"></input>
-                <input placeholder='anything'
+                <input placeholder='Currency'
               className="border rounded w-full py-2 px-3"
               type="text"></input>
-                <input placeholder='anything'
-              className="border rounded w-full py-2 px-3"
-              type="text"></input>
+              {/*onClick={callAPI}*/} 
               <button className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Calculate</button>
           </div>
         </div>
