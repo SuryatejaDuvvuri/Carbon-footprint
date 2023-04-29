f8837e60dcfd72d5b99c994fd503eda5578d68f2
SuryatejaDuvvuri <duvvurisuryateja95@gmail.com>
2023-04-29 11:28:42 -0700

    Add react-icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { AiFillHome } from "react-icons/ai";
+import { IoShareSocialSharp } from 'react-icons/io5'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <h1 className='text-red-500'>Hello world!</h1> 
+      <div className = "flex-col p-6 max-w-sm mx-auto ">
+          {/* <div className = "flex-row items-center space-x-4">
+            <AiFillHome href = "/"/>
+            <IoShareSocialSharp />
+          </div> */}
+        {/* Social sharing & Icons */}
+        {/* Carbon calculator */}
+        {/* Results based on user-input - button event listener */}
+      </div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </main>
   )
 }
